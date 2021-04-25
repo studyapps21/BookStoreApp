30416e373822b7218f73fd90099b2fb5192f63b6
pragmeht <pragnesh.tutorials>
2021-04-25 12:08:22 +0530

    Enable Razor cruntime compilation

--- Prag.BookStore/Startup.cs
+++ Prag.BookStore/Startup.cs
@@ -26,6 +26,10 @@ namespace Prag.BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            #if DEBUG
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            #endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
