2cad40974d11c016fdda13a062eb619f730220e1
pragmeht <pragnesh.tutorials>
2021-04-25 11:32:28 +0530

    Add project files.

---
+++ Prag.BookStore/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prag.BookStore.Models;
+using Prag.BookStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prag.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ActionResult getAllBooks()
+        {
+           var data= _bookRepository.GetAllBooks();
+
+           return View();
+        }
+
+        public BookModel getBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> searchBooks(string bookName , string authorName)
+        {
+            List<BookModel> abc = _bookRepository.SearchBook(bookName, authorName);
+            return abc;
+        }
+    }
+}
